a9101568e80fb7122444c7983300c061dd8ef8a6
niieeer <85437955+niieeer@users.noreply.github.com>
2021-12-10 09:34:14 +0100

    Affichage film populaire et affichage en fonction de l'id (#4)

--- index.php
+++ index.php
@@ -5,28 +5,27 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+    </style> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/style/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <title>Wati-Stream</title>
-    <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
-</style> 
 </head>
 
 <body>
-
-    <!-- Mon header -->
+     <!-- Mon header -->
   <?php include_once ('./includes/header.php'); ?> 
+    <main>
+        <div class="movies_container">
 
-
-
-    <!-- Si connecter afficher deconnexion -->
-    <?php if (isset($_SESSION['pseudo'])) { ?>
-        <a href="http://localhost/tp_film/controller/deconnexion.php"><i class="fas fa-sign-out-alt"></i> Deconnexion</a>
-        <p>Salut fils de pute !</p>
-    <?php  } ?>
-    <?php include_once ('./includes/footer.php'); ?> 
+        </div>
+    </main>
+  <?php include_once ('./includes/footer.php'); ?> 
+  
+      <!-- Script js pour fetch les films -->
+  <script src="./js/getMovies.js"></script>
 </body>
-
 </html>
---
+++ views/movie.php
@@ -0,0 +1,29 @@
+<?php $movie_id = htmlspecialchars(strip_tags($_GET["id"]));
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <div class="movie-container">
+            <a href="">
+
+            </a>
+        </div>
+    </div>
+</body>
+
+<script src="../js/getId.js"></script>
+<script src="../js/getMovie.js"></script>
+
+</html>
