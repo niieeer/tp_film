3b623a18cd76483ea2f1d28e4cf321d6759ed881
IDreacks <m-jouan@hotmail.com>
2021-12-10 09:24:28 +0100

    CSS maxime

--- index.php
+++ index.php
@@ -6,15 +6,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <title>Wati-Stream</title>
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+</style> 
 </head>
 
 <body>
+
+    <!-- Mon header -->
+  <?php include_once ('./includes/header.php'); ?> 
+
+
+
     <!-- Si connecter afficher deconnexion -->
     <?php if (isset($_SESSION['pseudo'])) { ?>
         <a href="http://localhost/tp_film/controller/deconnexion.php"><i class="fas fa-sign-out-alt"></i> Deconnexion</a>
         <p>Salut fils de pute !</p>
     <?php  } ?>
+    <?php include_once ('./includes/footer.php'); ?> 
 </body>
 
 </html>
--- views/inscription.php
+++ views/inscription.php
@@ -5,30 +5,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+    </style>
 </head>
 
 <body>
-    <form action="../controller/inscription.php?type=add" method="post">
-        <h2>Inscription</h2>
-        <label>Pseudo :</label>
-        <input type="text" name="user" autofocus maxlength="15" pattern="^[a-z0-9]+$" required>
-        <label>Mot de passe :</label>
-        <input type="password" name="pass" id="" required>
-        <label>Confirmer le mot de passe :</label>
-        <input type="password" name="confpass" id="confirm_pass" required>
-        <label>Email :</label>
-        <input type="email" name="mail" id="" required>
-        <div id="odd">
-            <p>En créant votre compte, vous acceptez nos
-                <u>termes et conditions </u>
-                <u> & politique de
-                    confidentialité </u>
-            </p>
-            <input class="sendButton" type="submit" value="Envoyer">
-        </div>
-    </form>
+    <?php include_once('../includes/header.php'); ?>
+    <div id="inscription">
+        <form class="inscription" action="../controller/inscription.php?type=add" method="post">
+            <h2>Inscription</h2>
+            <label>Pseudo :</label>
+            <input type="text" name="user" autofocus maxlength="15" pattern="^[a-z0-9]+$" required placeholder="xxtristanxx">
+            <label>Mot de passe :</label>
+            <input type="password" name="pass" id="" required placeholder="*******">
+            <label>Confirmer le mot de passe :</label>
+            <input type="password" name="confpass" id="confirm_pass" required placeholder="*******">>
+            <label>Email :</label>
+            <input type="email" name="mail" id="" required placeholder="tristan@gmail.com">
+            <div id="odd">
+
+                <input class="sendButton" type="submit" value="Envoyer">
+                <p>En créant votre compte, vous acceptez nos
+                    <span> <u>termes et conditions </u>
+                        <u> & politique de
+                            confidentialité </u> </span>
+                </p>
+            </div>
+        </form>
     </div>
+    <?php include_once('../includes/footer.php'); ?>
 </body>
 
 </html>
